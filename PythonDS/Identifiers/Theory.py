d7dd4f8a38e4c8b1c9608529e6964b198c724f73
Asna V A <asnava3131@gmail.com>
2025-02-05 12:51:54 +0530

    ...

---
+++ PythonDS/Identifiers/Theory.py
@@ -0,0 +1,13 @@
+#Identifiers - name or identity used in program, It may be variable names, Function names, and class names.
+
+#Variables - Used to assign a value
+
+#syntax: variable_name = value  (Dynamic typed PL - Python)
+
+num = 10
+age = 24
+mark = 40.5
+name = 'Asna'
+
+# - Used to comment single line
+# To comment multiple lines control + / and to uncomment control + /
